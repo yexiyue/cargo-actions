62a160e834a23a9514f975e905c1a636cd7fedf4
yexiyue <yexiyue666@qq.com>
2024-01-28 21:01:43 +0800

    feat: support cargo

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,21 @@ mod config;
 mod init;
 pub mod utils;
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
 pub trait Run {
     fn run(&self) -> anyhow::Result<()>;
 }
 
 #[derive(Debug, Parser)]
-#[command(author,version,about,long_about = None)]
-pub struct CargoAction {
-    #[command(subcommand)]
-    subcommand: Commands,
+#[command(author, version, about, name = "cargo actions", bin_name = "cargo")]
+pub enum CargoAction {
+    #[command(subcommand, name = "actions")]
+    Actions(Commands),
 }
 
-#[derive(Debug, Subcommand)]
-enum Commands {
+#[derive(Debug, Parser)]
+pub enum Commands {
     /// Init a github actions workflow
     Init {
         #[arg(default_value = "yexiyue/cargo-actions")]
@@ -26,7 +26,9 @@ enum Commands {
 
 impl Run for CargoAction {
     fn run(&self) -> anyhow::Result<()> {
-        Ok(self.subcommand.run()?)
+        match &self {
+            Self::Actions(action) => action.run(),
+        }
     }
 }
 
