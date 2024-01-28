c7b0344974b5d8807be574bd08ba1a5b5cc704cc
yexiyue <yexiyue666@qq.com>
2024-01-28 19:47:51 +0800

    feat: add spinner

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,12 +1,21 @@
 use dialoguer::theme;
 use git2;
-use std::{ffi::OsStr, fs::File};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::{ffi::OsStr, fs::File, time::Duration};
 use tempfile::tempdir;
 use walkdir;
 
 use crate::config::Config;
 
 pub fn init(name: Option<String>) -> anyhow::Result<()> {
+    let spinner = ProgressBar::new_spinner();
+    spinner.set_style(
+        ProgressStyle::default_spinner()
+            .tick_strings(&["/", "|", "\\", "-", "/"])
+            .template("{spinner:.green} {msg:.blue} {elapsed:>10.yellow}")?,
+    );
+    spinner.enable_steady_tick(Duration::from_millis(100));
+    spinner.set_message("🚀 Cloning...");
     let git_name = name.unwrap();
     let url = format!("https://github.com/{git_name}.git");
     let dir = tempdir()?;
@@ -24,7 +33,7 @@ pub fn init(name: Option<String>) -> anyhow::Result<()> {
         let config = serde_json::from_reader(file)?;
         configs.push(config);
     }
-
+    spinner.finish_and_clear();
     let result = cargo_actions.iter().zip(configs.iter()).collect::<Vec<_>>();
 
     let choice = dialoguer::Select::with_theme(&theme::ColorfulTheme::default())
